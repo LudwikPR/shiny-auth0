c8075b10c75396d27aeaf37546bfe887e64a0188
LudwikPR <25858137+LudwikPR@users.noreply.github.com>
2017-10-17 22:27:13 -0400

    Patching holes

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,11 @@ var env = {
   AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
 }
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.redirect('');
+});
+
 router.get('/login',
   function(req, res){
     res.render('login', { env: env });
